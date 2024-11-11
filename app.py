480e74d2a5f1770284f57bccb6532fd608781dc4
sasaki <Yumeko.Sasaki@jp.ricoh.com>
2024-11-11 13:04:01 +0900

    ルーティング変数の実装

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, こんにちは</p>"
 
-@app.route("/sub1")
-def hello_sub1():
-    return "<p>sub1ページです</p>"
+@app.route("/<name>")
+def hello_sub1(name):
+    return f"<p>{name}ページです</p>"
 
 @app.route("/sub2")
 def hello_sub2():
