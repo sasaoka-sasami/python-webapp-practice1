a6291c84bc13984bdae34fded102a1787175716a
sasaki <Yumeko.Sasaki@jp.ricoh.com>
2024-11-11 13:16:46 +0900

    画面内条件分岐を実装

--- app.py
+++ app.py
@@ -3,17 +3,25 @@ from flask import render_template
 
 app = Flask(__name__)
 
+list = [
+    'test1',
+    'test2',
+    'test3',
+    'test4',
+    'test5'
+]
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, こんにちは</p>"
 
-@app.route("/<name>")
-def hello_sub1(name):
-    return f"<p>{name}ページです</p>"
+# @app.route("/<name>")
+# def hello_sub1(name):
+#     return f"<p>{name}ページです</p>"
 
-@app.route("/sub2")
-def hello_sub2():
-    return render_template('index.html')
+@app.route("/<name>")
+def hello_sub2(name):
+    return render_template('index.html', name=name, list=list)
 
 if __name__ == "__main__":
     app.run()
