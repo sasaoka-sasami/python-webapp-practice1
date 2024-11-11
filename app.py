8c52c3943c1a70bdf4bbfc0aa05aa20e7241abf0
sasaki <Yumeko.Sasaki@jp.ricoh.com>
2024-11-11 11:58:08 +0900

    sub1とsub2へのルーティングを追加

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, こんにちは</p>"
 
+@app.route("/sub1")
+def hello_sub1():
+    return "<p>sub1ページです</p>"
+
+@app.route("/sub2")
+def hello_sub2():
+    return "<p>sub2ページです</p>"
+
 if __name__ == "__main__":
     app.run()
